45f1ee0d1d047eff9d0b36dcd9632579be720a8a
ArifAhmed <arif15-12301@diu.edu.bd>
2022-01-24 15:52:12 +0600

    Solved_1150
    Seen Solution for Solving this problem

--- beecrowd/src/beecrowd/BEE_1150.java
+++ beecrowd/src/beecrowd/BEE_1150.java
@@ -8,22 +8,29 @@ public class BEE_1150 {
 		// TODO Auto-generated method stub
 		//READ TWO INTEGERS
 		Scanner in = new Scanner(System.in);
-		int X;
+		int X = in.nextInt();//21//3
+		int Z = in.nextInt();//21//1
 		int i = 0;
+		int j = 0;
 		//Z MUST BE READ UNTIL BIGGER X APPEARS
-		for(;;) {
+		while(Z<=X) {//1<=3
+			Z = in.nextInt();//21=21//20
+		}
+		while(j<Z) {//30,43>30 so stop//0<20
+			j+=X;//j = j + X; j = 0+21, j = 21 + 22 = 43// j = 0+3, j = 3+4 =7+5=12+6=18+7=25
+			i++;//1+1 = 2; Ans...//1+1+1+1+1
+			X++;//22+1 = 23;//4+1+1+1+1
+		}
+		System.out.println(i);
+
 			
-			X = in.nextInt();//3
-			for(int Z = in.nextInt(); Z <X;Z++) {
-				i++;
+		
 				
-				
-				
-			}
-			System.out.println(i);
-			break;
+			
+			
+			
 		}
 
 	}
 
-}
+
